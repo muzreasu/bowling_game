bfa0d5d29ebd88fb5241dc576bd87eef4b48f652
Xiaofeng Su <xiaofeng.su@thoughtworks.com>
2020-06-03 23:13:23 +0800

    can calculate total score when have spare

--- main/BowlingGame.js
+++ main/BowlingGame.js
@@ -1,17 +1,38 @@
+const ROUND_FULL_SCORE = 10;
+
 export class Round {
   constructor(firstScore, secondScore, thirdScore) {
     this.firstScore = firstScore;
     this.secondScore = secondScore;
     this.thirdScore = thirdScore;
   }
+
+  getTotalScore() {
+    return this.firstScore + this.secondScore;
+  }
 }
 
 export const calculateTotalScore = (game) => {
   let lastRound = game[game.length - 1];
-  if (game.every((round) => round.firstScore === 10)) {
+  let totalScore = 0;
+  const isAllStrike = game.every((round) => round.firstScore === ROUND_FULL_SCORE);
+
+  if (isAllStrike) {
     return game.map((round) => round.firstScore)
       .reduce((accumulateScore, currentScore) => accumulateScore + currentScore * 3) + lastRound.secondScore + lastRound.thirdScore;
   }
-  return game.map((round) => round.firstScore + round.secondScore)
-    .reduce((accumulateScore, currentScore) => accumulateScore + currentScore) + lastRound.thirdScore;
+
+  for (let i = 0; i < 10; i++) {
+    const isSpare = game[i].getTotalScore() === ROUND_FULL_SCORE;
+    if (isSpare) {
+      if (i === 9 && game[i].thirdScore !== 0) {
+        totalScore += ROUND_FULL_SCORE + game[i].thirdScore;
+      } else {
+        totalScore += ROUND_FULL_SCORE + game[i + 1].firstScore;
+      }
+    } else {
+      totalScore += game[i].getTotalScore();
+    }
+  }
+  return totalScore;
 };
--- test/bowling.test.js
+++ test/bowling.test.js
@@ -1,6 +1,16 @@
 import { calculateTotalScore, Round } from "../main/BowlingGame";
+import { cloneDeep } from 'lodash';
 
 describe('bowling scoring rules', () => {
+  const initialGame = [];
+  for (let i = 0; i < 2; i++) {
+    initialGame.push(new Round(1, 4, 0));
+    initialGame.push(new Round(3, 5, 0));
+    initialGame.push(new Round(4, 4, 0));
+    initialGame.push(new Round(2, 3, 0));
+    initialGame.push(new Round(1, 4, 0));
+  }
+
   test('should return 300 when calculate total score given ten rounds are all strike', () => {
     const game = [];
     for (let i = 0; i < 9; i++) {
@@ -12,15 +22,15 @@ describe('bowling scoring rules', () => {
   });
 
   test('should return 62 when calculate total score given ten rounds are [(1,4),(3,5),(4,4),(2,3),(1,4),(1,4),(3,5),(4,4),(2,3),(1,4)]', () => {
-    const game = [];
-    for (let i = 0; i < 2; i++) {
-      game.push(new Round(1, 4, 0));
-      game.push(new Round(3, 5, 0));
-      game.push(new Round(4, 4, 0));
-      game.push(new Round(2, 3, 0));
-      game.push(new Round(1, 4, 0));
-    }
+    const game = cloneDeep(initialGame);
 
     expect(calculateTotalScore(game)).toBe(62);
+  });
+
+  test('should return 70 when calculate total score given ten rounds are [(4,6),(3,5),(4,4),(2,3),(1,4),(1,4),(3,5),(4,4),(2,3),(1,4)]', () => {
+    const game = cloneDeep(initialGame);
+    game[0] = new Round(4, 6);
+
+    expect(calculateTotalScore(game)).toBe(70);
   });
 });
