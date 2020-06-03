1a7196665a453407fb8a35c560ef036903d7f913
Xiaofeng Su <xiaofeng.su@thoughtworks.com>
2020-06-03 22:44:57 +0800

    can calculate all have no spare or strike

--- main/BowlingGame.js
+++ main/BowlingGame.js
@@ -8,6 +8,10 @@ export class Round {
 
 export const calculateTotalScore = (game) => {
   let lastRound = game[game.length - 1];
-  return game.map((round) => round.firstScore)
-    .reduce((accumulateScore, currentScore) => accumulateScore + currentScore * 3) + lastRound.secondScore + lastRound.thirdScore;
+  if (game.every((round) => round.firstScore === 10)) {
+    return game.map((round) => round.firstScore)
+      .reduce((accumulateScore, currentScore) => accumulateScore + currentScore * 3) + lastRound.secondScore + lastRound.thirdScore;
+  }
+  return game.map((round) => round.firstScore + round.secondScore)
+    .reduce((accumulateScore, currentScore) => accumulateScore + currentScore) + lastRound.thirdScore;
 };
--- test/bowling.test.js
+++ test/bowling.test.js
@@ -9,5 +9,18 @@ describe('bowling scoring rules', () => {
     game.push(new Round(10, 10, 10));
 
     expect(calculateTotalScore(game)).toBe(300);
-  })
+  });
+
+  test('should return 62 when calculate total score given ten rounds are [(1,4),(3,5),(4,4),(2,3),(1,4),(1,4),(3,5),(4,4),(2,3),(1,4)]', () => {
+    const game = [];
+    for (let i = 0; i < 2; i++) {
+      game.push(new Round(1, 4, 0));
+      game.push(new Round(3, 5, 0));
+      game.push(new Round(4, 4, 0));
+      game.push(new Round(2, 3, 0));
+      game.push(new Round(1, 4, 0));
+    }
+
+    expect(calculateTotalScore(game)).toBe(62);
+  });
 });
